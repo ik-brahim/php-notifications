a5e301d227298a67683775f9f7334c7a7368b6da
ik brahim <brahim.mailbox@gmail.com>
2020-01-08 04:14:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
      </div>
      <ul class="nav navbar-nav navbar-right">
       <li class="dropdown">
-       <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
+       <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <span class="label label-pill label-danger count" style="border-radius:10px;"></span> 
+        <span class="glyphicon glyphicon-bell" style="font-size:18px;"></span></a>
        <ul class="dropdown-menu"></ul>
       </li>
      </ul>
